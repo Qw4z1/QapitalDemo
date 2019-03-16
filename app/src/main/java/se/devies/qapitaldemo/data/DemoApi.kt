792edc0f27390262f2eb15746aa2a1669290f36e
Viktor Nyblom <viktor@devies.se>
2019-03-16 13:26:41 +0100

    Initial commit with api and response models

---
+++ app/src/main/java/se/devies/qapitaldemo/data/DemoApi.kt
@@ -0,0 +1,51 @@
+package se.devies.qapitaldemo.data
+
+import io.reactivex.Single
+import org.joda.time.DateTime
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+//http://qapital-ios-testtask.herokuapp.com/
+
+interface DemoApi {
+    @GET("/savingsgoals")
+    fun getSavingsGoals(): Single<List<SavingsGoal>>
+
+    @GET("/savingsrules")
+    fun getSavingsRules(): Single<List<SavingsRule>>
+
+    @GET("/savingsgoals/{id}/feed")
+    fun getFeed(@Path("id") id: Int): Single<FeedWrapper>
+}
+
+data class SavingsGoal(
+    val goalImageURL: String,
+    val userId: Int,
+    val targetAmount: Float,
+    val currentBalance: Float,
+    val status: String,
+    val name: String,
+    val id: Int,
+    val created: IntArray,
+    val connectedUsers: List<Int>?
+)
+
+data class SavingsRule(
+    val id: Int,
+    val type: String,
+    val amount: Int
+)
+
+data class FeedWrapper(
+    val feed: Feed
+)
+
+data class Feed(
+    val id: String,
+    val type: String,
+    val timestamp: DateTime,
+    val message: String,
+    val amount: Float,
+    val userId: Int,
+    val savingsRuleId: Int
+)
---
+++ app/src/main/java/se/devies/qapitaldemo/data/DemoRepo.kt
@@ -0,0 +1,7 @@
+package se.devies.qapitaldemo.data
+
+class DemoRepo(private val demoApi: DemoApi) {
+
+
+
+}
